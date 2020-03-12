fd1060cf762a17c90e7a7cd3869805cb1d8bc697
thilohuellmann <thilo@colabel.com>
2020-03-12 15:55:59 +0100

    removed dependencies

--- endpoint.py
+++ endpoint.py
@@ -1,17 +1,1 @@
-import os
-import uvicorn
-from fastapi import FastAPI
-from starlette.responses import UJSONResponse
-
-prefix = os.getenv('SCRIPT_PREFIX', '')
-app = FastAPI()
-
-
-@app.get(prefix + "/", response_class=UJSONResponse)
-async def hello_world():
-    """Hello world to check endpoint health"""
-    return {"message": "hello world"}
-
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="info")
+print('hello world')
